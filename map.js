bec69fe98bbd4137b3c712230c31947bdca289d3
AxSch <alex_schuneman@outlook.com>
2019-03-20 00:51:12 +0000

    feat: Added an accessToken & tileLayer

--- map.js
+++ map.js
@@ -1,2 +1,11 @@
 // setView takes coords latlng, second param is zoomLevel
-var map = L.map('map').setView([64.2008 , 149.4937], 13)
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+var accessToken = "pk.eyJ1IjoiYXhzY2giLCJhIjoiY2p0MDJuYTY5MDkzNDN6cXRzZHNuYXc4ZSJ9.-hDNKmsyjn3ztZhF2zuUCQ"
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: accessToken
+}).addTo(map);
